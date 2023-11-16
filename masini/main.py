d4e1ceb8067a5f5cd771fead0628ef1b42ad8617
unknown <gavrilasflorin@yahoo.com>
2023-11-16 18:47:17 +0200

    Adaug toate celelalte module

--- masini/main.py
+++ masini/main.py
@@ -1,5 +1,23 @@
 from afisari import * 
+from calcule import * 
+from conversii import * 
+from modificare import * 
 
-print(afisare_masini_categorie('M2'))
-print(afisare_masini_judet('ALBA'))
-print(afisare_masini_nr())
+
+# print(afisare_masini_categorie('M2'))
+# print(afisare_masini_judet('ALBA'))
+# print(afisare_masini_nr())
+
+# calculeaza_total_masini_catcom()
+# calculeaza_total_masini_comb()
+# calculeaza_total_masini_judet()
+# calculeza_total_masini()
+
+
+# adauga_coloana_noua()
+# modificare_benzina()
+# modificare_judet()
+
+# converteste_fisier()
+# converteste_fisier_text()
+# converteste_json_to_csv()
